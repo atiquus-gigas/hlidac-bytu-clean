e0b473b67050ff1d919d611396a05ea4502f4b87
atiquus-gigas <ivan.jemelka@gmail.com>
2026-09-20 15:05:11 +0200

    Add scraper function for Maxima property listings

---
+++ sources /maxima.py
@@ -0,0 +1,35 @@
+# sources/maxima.py
+from bs4 import BeautifulSoup
+import re
+
+def fetch(scraper):
+    url = "https://www.maxima.cz/nemovitosti/pronajem-bytu/praha"
+    html = scraper.get(url)
+    soup = BeautifulSoup(html, "html.parser")
+
+    offers = []
+
+    items = soup.select("div.property")
+    for item in items:
+        try:
+            titulek = item.select_one("h2").get_text(strip=True)
+            cena = item.select_one(".price").get_text(strip=True)
+            lokalita = item.select_one(".location").get_text(strip=True)
+            odkaz = item.select_one("a")["href"]
+
+            eid = "maxima_" + re.sub(r"\D", "", odkaz)
+
+            offers.append({
+                "portal": "Maxima",
+                "eid": eid,
+                "titulek": titulek,
+                "cena": cena,
+                "lokalita": lokalita,
+                "patro": "",
+                "vytah": False,
+                "odkaz": odkaz,
+            })
+        except:
+            continue
+
+    return offers
